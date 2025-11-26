f1087ba29c191732711621a931beb3d5c72325f5
Rafael Uchôa <r.da-costa-uchoa@guideline-contractors.com>
2025-11-26 15:26:44 -0300

    feat: add has_many columns

--- app/views/default_cruds/index.rb
+++ app/views/default_cruds/index.rb
@@ -14,12 +14,46 @@ module Views
       def setup_columns(table)
         model_class = data.model
 
+        # Adiciona colunas de atributos
         model_class.attribute_names.each do |attr|
           add_column(table, attr, model_class)
+        end
+
+        # Adiciona colunas de associações (has_many, has_one, etc)
+        model_class.reflect_on_all_associations.each do |association|
+          # Pula belongs_to pois já é exibido como atributo (foreign_key)
+          next if association.macro == :belongs_to
+
+          add_association_column(table, association)
+        end
+      end
+
+      def add_association_column(table, association)
+        column_name = association.klass.model_name.human(count: 2)
+
+        table.column(column_name) do |item|
+          related_objects = item.public_send(association.name)
+
+          case association.macro
+          when :has_many, :has_and_belongs_to_many
+            "#{related_objects.count} #{association.klass.model_name.human(count: related_objects.count)}"
+          when :has_one
+            related_objects&.try(:name) || related_objects&.try(:title) || related_objects&.to_s
+          end
         end
       end
 
       def add_column(table, attr, model_class)
+        # Pula colunas de foreign keys, pois serão tratadas como associações
+        if attr.end_with?("_id")
+          association_name = attr.delete_suffix("_id")
+          association = model_class.reflect_on_association(association_name.to_sym)
+          if association && association.macro == :belongs_to
+            add_belongs_to_column(table, association)
+            return
+          end
+        end
+
         column_name = model_class.human_attribute_name(attr)
 
         table.column(column_name) do |item|
@@ -31,23 +65,17 @@ module Views
         end
       end
 
-      def add_column(table, attr, model_class)
-        column_name = model_class.human_attribute_name(attr)
+      def add_belongs_to_column(table, association)
+        column_name = association.klass.model_name.human
 
         table.column(column_name) do |item|
-          if attr == "id"
-            id_link(item)
-          else
-            value = item.public_send(attr)
+          related_object = item.public_send(association.name)
+          next if related_object.nil?
 
-            # Verifica se é uma associação
-            association = model_class.reflect_on_association(attr.remove("_id").to_sym)
-            if association
-              format_association(value, association)
-            else
-              format_value(value)
-            end
-          end
+          related_object.try(:name) ||
+          related_object.try(:title) ||
+          related_object.try(:email) ||
+          "#{association.klass.model_name.human} ##{related_object.id}"
         end
       end
 
